2a3332fa3e0cc2713b66e876b7d6c3cc948bd95f
Andrew <62994051+Rarmash@users.noreply.github.com>
2022-06-09 17:41:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ intents = discord.Intents.default()
 intents.members=True
 intents.messages=True
 
-bot = commands.Bot(command_prefix='?', case_insensitive=True)
+bot = commands.Bot(command_prefix='!', case_insensitive=True)
 
 @bot.event
 async def on_ready():
