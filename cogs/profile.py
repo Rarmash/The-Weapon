7e13192ef3282a132c1de36c5b2ad3536d4b2da0
Andrew <62994051+Rarmash@users.noreply.github.com>
2022-06-13 21:28:52 +0300

    Bot profile update

--- cogs/profile.py
+++ cogs/profile.py
@@ -10,23 +10,39 @@ class Profile(commands.Cog):
         date_format = "%#d.%#m.%Y в %H:%M"
         if user is None:
             user = ctx.author
-            
-        if user.status == discord.Status.online:
-            status = "В сети"
-        if user.status == discord.Status.offline:
-            status = "⚪ Не в сети"
-        if user.status == discord.Status.idle:
-            status = "Не активен"
-        if user.status == discord.Status.dnd:
-            status = "Не беспокоить"
-        
-        embed = discord.Embed(title = f'Карточка {user.name}', description=f"Тег: <@{user.id}>", color = 0x209af8)
-        embed.add_field(name = "Регистрация", value = user.created_at.strftime(date_format))
-        embed.add_field(name = "На сервере с", value = user.joined_at.strftime(date_format))
-        #embed.add_field(name="Наивысшая роль", value=user.top_role.mention)
-        #embed.add_field(name = "Статус", value = user.status)
-        embed.set_thumbnail(url=user.avatar_url)
-        await ctx.send(embed = embed)
+        if user.id != self.bot.user.id:
+            if user.status == discord.Status.online:
+                status = "В сети"
+            if user.status == discord.Status.offline:
+                status = "⚪ Не в сети"
+            if user.status == discord.Status.idle:
+                status = "Не активен"
+            if user.status == discord.Status.dnd:
+                status = "Не беспокоить"
+            embed = discord.Embed(title = f'Карточка {user.name}', description=f"Тег: <@{user.id}>", color = 0x209af8)
+            embed.add_field(name = "Регистрация", value = user.created_at.strftime(date_format))
+            embed.add_field(name = "На сервере с", value = user.joined_at.strftime(date_format))
+            #embed.add_field(name = "Статус", value = user.status)
+            embed.set_thumbnail(url=user.avatar_url)
+            await ctx.send(embed = embed)
+        if user.id == self.bot.user.id:
+            if user.status == discord.Status.online:
+                status = "В сети"
+            if user.status == discord.Status.offline:
+                status = "⚪ Не в сети"
+            if user.status == discord.Status.idle:
+                status = "Не активен"
+            if user.status == discord.Status.dnd:
+                status = "Не беспокоить"
+            embed = discord.Embed(title = f'Карточка {user.name}', description=f"Тег: <@{user.id}>", color = 0x209af8)
+            embed.add_field(name = "Владелец", value = f"<@390567552830406656>")
+            embed.add_field(name = "Сервер бота", value = "Rebox Shit Force")
+            embed.add_field(name = "Создан", value = user.created_at.strftime(date_format))
+            embed.add_field(name = "На сервере с", value = user.joined_at.strftime(date_format))
+            embed.add_field(name = "Статус", value = status)
+            embed.add_field(name = "Приглашение", value = "[Тык](https://discord.com/oauth2/authorize?client_id=935560968778448947&scope=bot&permissions=8)")
+            embed.set_thumbnail(url=user.avatar_url)
+            await ctx.send(embed = embed)
 
 
 def setup(bot):
---
+++ cogs/service.py
@@ -0,0 +1,48 @@
+import discord
+from discord.ext import commands
+from options import admin_id as administrator
+import os
+from time import time
+
+class Service(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def ping(self, ctx):
+        st = time()
+        message = await ctx.send(f"Понг! :ping_pong: Задержка: {self.bot.latency*1000:,.0f} ms.")
+        end = time()
+        await message.edit(content=f"Понг! :ping_pong: Задержка: {self.bot.latency*1000:,.0f} ms. Задержка DWSP: {(end-st)*1000:,.0f} ms.")
+
+    @commands.command()
+    async def avatar(self, ctx):
+        author = ctx.message.author
+        
+        embed = discord.Embed(title=f"Аватарка {author}"[:-5],
+                                color=discord.Color.dark_gray())
+        link = author.avatar_url
+        embed.set_image(url = link)
+
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def hello(self, ctx):
+        author = ctx.message.author
+        await ctx.send(f'Привет, {author.mention}!')
+
+    @commands.command(pass_context=True)
+    async def say(self, ctx, arg):
+        await ctx.send(arg)
+
+    @commands.command()
+    async def shutdown(self, ctx):
+        if ctx.author.id == administrator:
+            await ctx.send("Завершение работы... :wave:")
+            os.abort()
+        else:
+            await ctx.send("Недостаточно прав для выполнения данной команды.")
+
+def setup(bot):
+    bot.add_cog(Service(bot))
