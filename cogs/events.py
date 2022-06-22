22d20b7a29c5ed6c9bff0e12a0c6b5f34776b31c
Andrew <62994051+Rarmash@users.noreply.github.com>
2022-06-22 22:44:29 +0300

    Cooldown update

--- cogs/events.py
+++ cogs/events.py
@@ -8,12 +8,16 @@ class Events(commands.Cog):
         self.bot = bot
     
     @commands.Cog.listener()
-    
     async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
         if isinstance(error, commands.MemberNotFound):
             await ctx.message.delete()
             embed = discord.Embed(description="❌ Пользователь не найден", color = 0xff0000)
-            await ctx.send(embed=embed, delete_after=5.0)
+            await ctx.respond(embed=embed, delete_after=5.0)
 
+    @commands.Cog.listener()
+    async def on_application_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            await ctx.respond(error, delete_after=5.0)
+    
 def setup(bot):
     bot.add_cog(Events(bot))
