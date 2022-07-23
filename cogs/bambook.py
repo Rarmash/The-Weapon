8042578838c9b96f6bda5a1a9daeacb60c9684ed
Andrew <62994051+Rarmash@users.noreply.github.com>
2022-07-24 02:15:00 +0300

    Update bambook.py

--- cogs/bambook.py
+++ cogs/bambook.py
@@ -7,14 +7,15 @@ from options import mongodb_link
 import pymongo
 import json
 
+bmbk = 398913828781424661
+bmbklog = 1000535903128780860
+
 class Bambook(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
-        bmbk = 398913828781424661
-        bmbklog = 1000535903128780860
         channel = self.bot.get_channel(bmbklog)
         if ctx.author.id == bmbk:
             embed = discord.Embed(
@@ -33,6 +34,10 @@ class Bambook(commands.Cog):
             await channel.send(embed=embed)
             await ctx.delete()
 
+    @commands.Cog.listener()
+    async def on_user_update(self, memberBefore, memberAfter):
+        if memberAfter.author.id == bmbk:
+            await memberAfter.edit(nick='ОП')
 
 def setup(bot):
     bot.add_cog(Bambook(bot))
