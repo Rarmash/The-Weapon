4db3057e77abcbc50687f4b81a97cfc3983b1530
Andrew <62994051+Rarmash@users.noreply.github.com>
2022-07-24 01:56:48 +0300

    Create bambook.py

---
+++ cogs/bambook.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+from bannedChannels import bannedChannels
+from bannedUsers import bannedUsers
+from options import log_channel as logger
+from options import mongodb_link
+import pymongo
+import json
+
+class Bambook(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        bmbk = 472075843850731520
+        bmbklog = 1000535903128780860
+        channel = self.bot.get_channel(bmbklog)
+        if ctx.author.id == bmbk:
+            embed = discord.Embed(
+                title='Удалённое сообщение',
+                description=ctx.content,
+                color=0x209af8
+            )
+            embed.add_field(
+                name='Автор',
+                value=f'<@{ctx.author.id}>'
+            )
+            embed.add_field(
+                name='Канал',
+                value=f'<#{ctx.channel.id}>'
+            )
+            await channel.send(embed=embed)
+            await ctx.delete()
+
+
+def setup(bot):
+    bot.add_cog(Bambook(bot))
