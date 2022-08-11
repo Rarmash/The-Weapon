f89900cf32ea0682857c14aa4994e180938b1dfc
Andrew <62994051+Rarmash@users.noreply.github.com>
2022-08-11 19:59:12 +0300

    Update poll.py

--- cogs/poll.py
+++ cogs/poll.py
@@ -2,9 +2,24 @@ import discord
 from discord.ext import commands
 
 class Poll(commands.Cog):
-    def __init__(self, client):
-        self.client = client
+    def __init__(self, bot):
+        self.Bot = bot
 
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        if payload.channel_id == 1007331086164049980:
+            channel = await self.Bot.fetch_channel(payload.channel_id)
+            message = await channel.fetch_message(payload.message_id)
+
+            # iterating through each reaction in the message
+            for r in message.reactions:
+
+                # checks the reactant isn't a bot and the emoji isn't the one they just reacted with
+                if payload.member in await r.users().flatten() and not payload.member.bot and str(r) != str(payload.emoji):
+
+                    # removes the reaction
+                    await message.remove_reaction(r.emoji, payload.member)
+    
     @commands.slash_command(description='Предложить идею')
     async def poll(self, ctx: discord.ApplicationContext, *, question):
         pollEmbed = discord.Embed(title = "Новое предложение", description = f"{question}", color = 0x209af8)
