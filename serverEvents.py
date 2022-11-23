325b31ce3d84c7724c941a319ede4476a13e210b
Andrew <62994051+Rarmash@users.noreply.github.com>
2022-11-24 01:26:00 +0300

    json events

--- serverEvents.py
+++ serverEvents.py
@@ -1,4 +1,16 @@
 #time is in GMT +0
-serverEventsDates = [
-    "21.10.2022 12:23", "09.12.2022 21:00" # Конкурс на новогоднюю аватарку для бота Оружие
-    ]
+import json
+
+with open("serverEvents.json", "r") as file:
+    data = json.load(file)
+
+serverEventsDates = []
+serverEventsTitles = []
+serverEventsDescriptions = []
+serverEventsIcons = []
+
+for i in data:
+    serverEventsTitles.append(data[i][0][u'title'])
+    serverEventsDescriptions.append(data[i][0][u'description'])
+    serverEventsIcons.append(data[i][0][u'icon'])
+    serverEventsDates.append(data[i][0][u'date'])
