a702d8f2edf598a0c7c3c55eaff09af23f284b57
Andrew <62994051+Rarmash@users.noreply.github.com>
2023-07-03 02:43:52 +0300

    Hotfix v2.6.0.1
    -Fixed connection bug

--- options.py
+++ options.py
@@ -2,12 +2,13 @@ from dotenv import load_dotenv
 from pathlib import Path
 import os
 import pymongo
+import certifi
 
 load_dotenv()
 env_path = Path('.')/'.env'
 load_dotenv(dotenv_path=env_path)
 
-version = "2.6.0"
+version = "2.6.0.1"
 
 token = os.environ["TOKEN"]
 mongodb_link = os.environ["MONGODB"]
@@ -26,6 +27,6 @@ mod_role_id = 646327704450236416
 insider_id = 986290766848602265
 admin_role_id = 646327510161686528
 
-myclient = pymongo.MongoClient(mongodb_link)
+myclient = pymongo.MongoClient(mongodb_link, tlsCAFile=certifi.where())
 Collection = myclient["Server"]["Users"]
 RolesCollection = myclient["Server"]["UserRoles"]
